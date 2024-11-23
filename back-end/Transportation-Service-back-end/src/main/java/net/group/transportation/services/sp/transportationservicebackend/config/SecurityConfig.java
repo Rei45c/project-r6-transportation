f16c2e31f1fa93984047a3a8e7f11dc3c304c4fc
OsmanKocak17 <150553278+OsmanKocak17@users.noreply.github.com>
2024-11-23 22:53:04 +0100

    spring enabled, every api point is accesible by everyone

---
+++ back-end/Transportation-Service-back-end/src/main/java/net/group/transportation/services/sp/transportationservicebackend/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package net.group.transportation.services.sp.transportationservicebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Optional, wenn nur lokal getestet wird
+                .authorizeRequests()
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll() // Erlaube /hello für alle
+                .requestMatchers(new AntPathRequestMatcher("/api/users/signup")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/users")).permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
